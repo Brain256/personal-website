cb2b13ee4c250516b5ba27d54e2a0dc9441b9674
Brain256 <brianyin256@gmail.com>
2025-09-10 14:49:59 -0400

    added navbar and basic ui

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+function Navbar() {
+
+    return (
+        <>
+            <div className="nav-container">
+                <div className="name">Brian Yin</div>
+                <div className="links">
+                    <div><a href="">Home</a></div>
+                    <div><a href="">Projects</a></div>
+                    <div><a href="">Experience</a></div>
+                    <div><a href="">Contact</a></div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
