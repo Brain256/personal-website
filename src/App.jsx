34543154ecb8ab16bad6a954432f0c66c00858e5
Brain256 <brianyin256@gmail.com>
2025-09-13 11:03:35 -0400

    added interactive background and changed contact info formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import Project from './components/Project.jsx'
 import ExpCard from './components/ExpCard.jsx'
+
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim"; 
 
 const projects = [
   {
@@ -57,11 +60,104 @@ const experiences = [
 
   }
 ]
+
 function App() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "transparent", // blue background
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+      particles: {
+        color: { value: "rgb(255, 255, 255)" },
+        links: {
+          color: "rgb(255,255,255)",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: { default: "bounce" },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: { enable: true },
+          value: 150,
+        },
+        opacity: { value: 0.5 },
+        shape: { type: "circle" },
+        size: { value: { min: 1, max: 5 } },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
 
   return (
     <>
+      
+      {init && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: -1,
+            pointerEvents: "none",
+          }}
+        />
+      )}
+
       <div className="page-container">
+        
+
         <div id="nav">
           <Navbar />
         </div>
@@ -70,16 +166,17 @@ function App() {
           <div id="header"> 
             <div className="text">
               <div className="intro">Hello, I'm Brian Yin</div>
-              <div className="intro-sub">First Year Computer Science Student at The University Of Waterloo</div>
+              <div className="intro-sub">
+                First Year Computer Science Student at The University Of Waterloo
+              </div>
             </div>
           </div>
           
-          
           <div id="project-container">
-              <h2>Projects</h2>
-              {projects.map((project, index) => (
-                <Project key={index} {...project} />
-              ))}
+            <h2>Projects</h2>
+            {projects.map((project, index) => (
+              <Project key={index} {...project} />
+            ))}
           </div>
 
           <div id="exp-container">
@@ -90,29 +187,30 @@ function App() {
           </div>
 
           <div id="contact-container">
-            <h2>Contact</h2>
+            <h2>Contact Me</h2>
             <div className="contact-links">
-              <a href="https://www.linkedin.com/in/brian-yin-1ab26a223/">
-                <img src="src/assets/linkedin.svg" width="50px" height="50px" alt="Linkedin"/>
-              </a>
-              <div>Brian Yin</div>
-              <a href="https://github.com/Brain256">
-                <img src="src/assets/github.svg" width="50px" height="50px" alt="Github"/>
-              </a>
-              <div>Brain256</div>
-              <a href="mailto:brianyin256@gmail.com">
+              <div>
+                <a href="https://www.linkedin.com/in/brian-yin-1ab26a223/">
+                  <img src="src/assets/linkedin.svg" width="50" height="50" alt="Linkedin"/>
+                </a>
+                <div>Brian Yin</div>
+              </div>
+              <div>
+                <a href="https://github.com/Brain256">
+                  <img src="src/assets/github.svg" width="50" height="50" alt="Github"/>
+                </a>
+                <div>Brain256</div>
+              </div>
+              <div>
                 <img src="src/assets/gmail.png" width="50" height="50" alt="Email" />
-              </a>
-              <div>brianyin256@gmail.com</div>
+                <div>brianyin256@gmail.com</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      
-      
-        
     </>
   )
 }
 
-export default App
+export default App;
