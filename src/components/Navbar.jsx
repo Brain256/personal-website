53b3685e2d36aab495fb6122ec72d5397bd0a598
Brain256 <brianyin256@gmail.com>
2025-09-13 22:17:01 -0400

    optimized responsive design for tablets and phones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,8 @@ function Navbar() {
     return (
         <>
             <div className="nav-container">
-                <div className="name"><img src="/Logo.jpg" height="50"/><div className="name-text">Brian Yin</div></div>
+                <div className="name"><a href="#header"><img src="/Logo.jpg" height="50"/></a><div className="name-text">Brian Yin</div></div>
                 <div className="links">
-                    <div><a href="#header">Home</a></div>
                     <div><a href="#project-container">Projects</a></div>
                     <div><a href="#exp-container">Experience</a></div>
                     <div><a href="#contact-container">Contact</a></div>
