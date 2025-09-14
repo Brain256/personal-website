f60dc9f44906fcf70d93223a7fe15afb8d516f1b
Brain256 <brianyin256@gmail.com>
2025-09-13 22:41:09 -0400

    further adjusted responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
                 <div className="name"><a href="#header"><img src="/Logo.jpg" height="50"/></a><div className="name-text">Brian Yin</div></div>
                 <div className="links">
                     <div><a href="#project-container">Projects</a></div>
-                    <div><a href="#exp-container">Experience</a></div>
+                    <div><a href="#exp-container">Career</a></div>
                     <div><a href="#contact-container">Contact</a></div>
                 </div>
             </div>
